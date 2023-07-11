3f877951dcf5e8badec0abb7be3b970487397c1d
Nicolass02 <nicolas.stocker02@gmail.com>
2023-07-11 16:03:33 +0200

    refined README

--- sequence_generator/tmap_generator.py
+++ sequence_generator/tmap_generator.py
@@ -3,9 +3,9 @@
 from tqdm import tqdm 
 
 import tmap as tm
-
+import jinja2
 import numpy as np
-
+import copy
 from faerun import Faerun
 
 import pandas as pd
@@ -31,6 +31,92 @@ if not file_path:
     
 
 filename = file_path.split("/")[-1:]
+
+#altered code from faerun
+def plot(
+    self,
+    file_name: str = "index",
+    path: str = "./",
+    template: str = "default",
+    notebook_height: int = 500,):
+    """Plots the data to an HTML / JS file.
+
+    Keyword Arguments:
+        file_name (:obj:`str`, optional): The name of the HTML / JS file
+        path (:obj:`str`, optional): The path to which to write the HTML / JS file
+        template (:obj:`str`, optional): The name or path of the template to use
+        notebook_height: (:obj`int`, optional): The height of the plot when displayed in a jupyter notebook
+    """
+    self.notebook_height = notebook_height
+
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    if template in ["default", "reaction_smiles", "smiles", "url_image"]:
+        template = "template_" + template + ".j2"
+    else:
+        script_path = os.path.dirname(template)
+
+    html_path = os.path.join(path, file_name + ".html")
+    js_path = os.path.join(path, file_name + ".js")
+    jenv = jinja2.Environment(loader=jinja2.FileSystemLoader(script_path))
+
+    has_legend = False
+
+    for _, value in self.scatters.items():
+        if value["has_legend"]:
+            has_legend = True
+            break
+
+    if not self.show_legend:
+        has_legend = False
+
+    # Drop colormaps before passing them to the document, as they are
+    # not JSON serializable.
+    trees_copy = copy.deepcopy(self.trees)
+    scatters_copy = copy.deepcopy(self.scatters)
+
+    for key, _ in trees_copy.items():
+        del trees_copy[key]["colormap"]
+
+    for key, _ in scatters_copy.items():
+        del scatters_copy[key]["colormap"]
+
+    model = {
+        "title": self.title,
+        "file_name": file_name + ".js",
+        "clear_color": self.clear_color,
+        "view": self.view,
+        "coords": str(self.coords).lower(),
+        "coords_color": self.coords_color,
+        "coords_box": str(self.coords_box).lower(),
+        "coords_ticks": str(self.coords_ticks).lower(),
+        "coords_grid": str(self.coords_grid).lower(),
+        "coords_tick_count": self.coords_tick_count,
+        "coords_tick_length": self.coords_tick_length,
+        "coords_offset": self.coords_offset,
+        "x_title": self.x_title,
+        "y_title": self.y_title,
+        "tree_helpers": list(trees_copy.values()),
+        "point_helpers": list(scatters_copy.values()),
+        "has_legend": str(has_legend).lower(),
+        "legend_title": self.legend_title,
+        "legend_orientation": self.legend_orientation,
+        "alpha_blending": str(self.alpha_blending).lower(),
+        "anti_aliasing": str(self.anti_aliasing).lower(),
+        "style": self.style,
+        "impress": self.impress,
+        "in_notebook": Faerun.in_notebook(),
+        "thumbnail_width": self.thumbnail_width,
+        "thumbnail_fixed": str(self.thumbnail_fixed).lower(),
+    }
+
+    
+    model["data"] = self.create_data()
+    
+
+    output_text = jenv.get_template(template).render(model)
+
+    with open(html_path, "w") as result_file:
+        result_file.write(output_text)
 
 
 #choose pickeled file
@@ -120,6 +206,6 @@ f.add_tree("TITLE_tree", {"from": s, "to": t}, point_helper="TITLE")
 filename = str(filename).strip(".pkl")
 
 #convert tmap to html file
-f.plot(f"tmaps/{filename}", template='smiles')
+plot(f"tmaps/{filename}", template='smiles')
 
 webbrowser.open('file://' + os.path.realpath(f"tmaps/{filename}.html"))
